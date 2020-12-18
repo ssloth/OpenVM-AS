63aec384dd6ab2ff7529a4b6c6fed7e04e681d8c
LiuZY <liuzeyu@star-net.cn>
2020-12-18 13:21:57 +0800

    feature: read file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 extern crate clap;
 
 use clap::{App, Arg};
+use std::path::Path;
+use std::process;
 
 fn main() {
-    let _matches = App::new("ovmas")
+    let matches = App::new("ovmas")
         .version("0.1.0")
         .author("LiuZY")
-        .about("open vm's assembler")
+        .about("open-vm's assembler")
         .arg(
             Arg::with_name("FILE")
+                .long("file")
+                .short("f")
                 .help("File to print.")
                 .empty_values(false),
         )
         .get_matches();
+
+    if let Some(file) = matches.value_of("FILE") {
+        println!("value for file argument: {}", file);
+        if Path::new(&file).exists() {
+            
+        } else {
+            eprintln!("error No such file");
+            process::exit(1);
+        }
+    }
 }
