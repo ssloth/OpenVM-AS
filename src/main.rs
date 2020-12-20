3154ded7c8f39693398c7d2f6a94ca8462af62b6
LiuZY <1035041075@qq.com>
2020-12-20 18:19:38 +0800

    feature: read file and compile file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 extern crate clap;
 
 use clap::{App, Arg};
+use std::fs::File;
+use std::io::*;
 use std::path::Path;
 use std::process;
+
+fn compile_line(line: &str) {
+    let words = line.split_whitespace();
+    for word in words {
+        print!("{} ", word);
+    }
+}
+
+fn compile_file(data: BufReader<File>) {
+    for line in data.lines() {
+        let line_str: String = line.unwrap();
+        let str = line_str.split(';').collect::<Vec<&str>>();
+        compile_line(str[0]);
+        println!();
+    }
+}
 
 fn main() {
     let matches = App::new("ovmas")
@@ -21,7 +39,9 @@ fn main() {
     if let Some(file) = matches.value_of("FILE") {
         println!("value for file argument: {}", file);
         if Path::new(&file).exists() {
-            
+            let f = File::open(file).expect("File not found");
+            let fin = BufReader::new(f);
+            compile_file(fin);
         } else {
             eprintln!("error No such file");
             process::exit(1);
